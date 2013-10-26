fdbf49bf93b55079fa87fac373008b84e1bc42c4
rafa-local <dragon1rafa@gmail.com>
2013-10-26 03:09:17 -0500

    Credito + .gitignore

--- protected/views/layouts/column2.php
+++ protected/views/layouts/column2.php
@@ -1,22 +1,24 @@
 <?php /* @var $this Controller */ ?>
 <?php $this->beginContent('//layouts/main'); ?>
-<div class="span-19">
+<div class="span-25">
+	
+	<div id="mainmenu">
+	<?php
+		//$this->beginWidget('zii.widgets.CPortlet', array(
+		//'title'=>'Operations',
+		//));
+		$this->widget('zii.widgets.CMenu', array(
+			'items'=>$this->menu,
+			//'htmlOptions'=>array('class'=>'operations'),
+		));
+		//$this->endWidget();
+	?>
+	</div><!-- sidebar -->
 	<div id="content">
 		<?php echo $content; ?>
 	</div><!-- content -->
 </div>
 <div class="span-5 last">
-	<div id="sidebar">
-	<?php
-		$this->beginWidget('zii.widgets.CPortlet', array(
-			'title'=>'Operations',
-		));
-		$this->widget('zii.widgets.CMenu', array(
-			'items'=>$this->menu,
-			'htmlOptions'=>array('class'=>'operations'),
-		));
-		$this->endWidget();
-	?>
-	</div><!-- sidebar -->
+	
 </div>
 <?php $this->endContent(); ?>
