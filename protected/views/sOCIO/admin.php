d351110d89126b8496748cd738d16fa9c333d14b
dimortizp <dimortizp@ieee.org>
2013-10-29 16:35:25 -0500

    remover update en admin

--- protected/views/sOCIO/admin.php
+++ protected/views/sOCIO/admin.php
@@ -2,14 +2,14 @@
 /* @var $this SOCIOController */
 /* @var $model SOCIO */
 
-$this->breadcrumbs=array(
-	'Socios'=>array('index'),
-	'Manage',
+$this->breadcrumbs = array(
+    'Socios' => array('index'),
+    'Manage',
 );
 
-$this->menu=array(
-	array('label'=>'Lista de socios', 'url'=>array('admin')),
-	array('label'=>'Crear Socio', 'url'=>array('create')),
+$this->menu = array(
+    array('label' => 'Lista de socios', 'url' => array('admin')),
+    array('label' => 'Crear Socio', 'url' => array('create')),
 );
 
 Yii::app()->clientScript->registerScript('search', "
@@ -29,41 +29,45 @@ $('.search-form form').submit(function(){
 <h1>Gestion de socios</h1>
 
 <p>
-Opciones de ordenamiento (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>)
+    Opciones de ordenamiento (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>)
 </p>
 
-<?php echo CHtml::link('Advanced Search','#',array('class'=>'search-button')); ?>
+<?php echo CHtml::link('Advanced Search', '#', array('class' => 'search-button')); ?>
 <div class="search-form" style="display:none">
-<?php $this->renderPartial('_search',array(
-	'model'=>$model,
-)); ?>
+    <?php
+    $this->renderPartial('_search', array(
+        'model' => $model,
+    ));
+    ?>
 </div><!-- search-form -->
 
-<?php $this->widget('zii.widgets.grid.CGridView', array(
-	'id'=>'socio-grid',
-	'dataProvider'=>$model->search(),
-	'filter'=>$model,
-	'columns'=>array(
-		'K_IDENTIFICACION',
-		'N_NOMBRE',
-		'N_APELLIDO',
-		//I_ESTADO_CIVIL',
-		//'N_OCUPACION',
-		'O_TARJETA_PROFESIONAL',
-		
-		//'I_GENERO',
-		//'O_DIRECCION_DOMICILIO',
-		//'O_DIRECCION_TRABAJO',
-		'O_CORREO_ELECTRONICO',
-		'O_TELEFONO_DOMICILIO',
-		'O_TELEFONO_TRABAJO',
-		'O_TELEFONO_CELULAR',
-		'F_INGRESO',
-		//'F_RETIRO',
-		//'O_CAUSAL_RETIRO',
-		
-		array(
-			'class'=>'CButtonColumn',
-		),
-	),
-)); ?>
+<?php
+$this->widget('zii.widgets.grid.CGridView', array(
+    'id' => 'socio-grid',
+    'dataProvider' => $model->search(),
+    'filter' => $model,
+    'columns' => array(
+        'K_IDENTIFICACION',
+        'N_NOMBRE',
+        'N_APELLIDO',
+        //I_ESTADO_CIVIL',
+        //'N_OCUPACION',
+        'O_TARJETA_PROFESIONAL',
+        //'I_GENERO',
+        //'O_DIRECCION_DOMICILIO',
+        //'O_DIRECCION_TRABAJO',
+        'O_CORREO_ELECTRONICO',
+        'O_TELEFONO_DOMICILIO',
+        'O_TELEFONO_TRABAJO',
+        'O_TELEFONO_CELULAR',
+        'F_INGRESO',
+        //'F_RETIRO',
+        //'O_CAUSAL_RETIRO',
+
+        array(
+            'class' => 'CButtonColumn',
+            'template' => '{view}',
+        ),
+    ),
+));
+?>
